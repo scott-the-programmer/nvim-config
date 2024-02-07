0dda0d9e676b13b1288bfd9b6af00294de75d523
Scott Murray <scott.murray@starshipit.com>
2024-02-07 13:24:19 +1300

    remove redundant map import

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local map = require("map").map
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
