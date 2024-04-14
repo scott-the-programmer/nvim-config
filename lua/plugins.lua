6fca99b3738a18feb8d7898a2043c0d6566793a1
Scott Murray <scott.murray@starshipit.com>
2024-04-15 08:46:33 +1200

    mocha

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -224,7 +224,7 @@ require('nvim-treesitter.configs').setup {
 }
 
 require("catppuccin").setup({
-  flavour = "macchiato", -- latte, frappe, macchiato, mocha
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
   background = {
     -- :h background
     light = "latte",
@@ -268,9 +268,7 @@ require("catppuccin").setup({
       enabled = true,
       indentscope_color = "",
     },
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
 })
 
--- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
